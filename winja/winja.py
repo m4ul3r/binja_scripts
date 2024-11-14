c952a89d3be7e7e6860ac1a165c870909fa14a76
m4ul3r <m4ul3r@protonmail.com>
2024-11-14 02:12:43 -0500

    improve speed

--- winja/winja.py
+++ winja/winja.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import time
 
 winja_path = Path.home() / "Documents" / "winja_bndbs"
 winja_path.mkdir(parents=True, exist_ok=True)
@@ -27,14 +28,19 @@ def add_analysis_to_bv(bv, dll_bv, dll_name):
         bv.define_user_type(name, typ)
     # merge functions
     for func in dll_bv.functions:
-        bv.add_function(func.start, func.platform)
+        bv.create_user_function(func.start, func.platform)
     # merge symbols
     for name, syms in dll_bv.symbols.items():
         for sym in syms:
             bv.define_auto_symbol(sym)
+    # TODO: merge external libraries
+    # for extern in bv.get_external_locations():
+	#     bv.add_external_location(extern.source_symbol, None, extern.target_symbol, extern.source_symbol.address)
+
 
 
 print("[+] Starting Merge")
+start = time.perf_counter()
 
 # skip binary and ntdll
 for mod in dbg.modules[1:-1]:
@@ -42,7 +48,7 @@ for mod in dbg.modules[1:-1]:
     winja_bndb = str(winja_path / dll_path.name) + ".bndb"
     # check if bndb is cache
     if Path(winja_bndb).exists():
-        dll_bv = load(winja_bndb)
+        dll_bv = load(winja_bndb, False)
         print(f"[+] Loaded bndb: {winja_bndb}")
     else:
         dll_bv = load(mod.name)
@@ -53,6 +59,7 @@ for mod in dbg.modules[1:-1]:
     dll_bv = dll_bv.rebase(mod.address, True)
     add_analysis_to_bv(bv, dll_bv, dll_path.name)
     print(f"[+] Added {dll_path.name} to bv")
+    dll_bv.file.close()
 
 # handle ntdll
 mod = dbg.modules[-1]
@@ -60,7 +67,7 @@ dll_path = Path(Path("C:\\Windows\\System32") / mod.name)
 winja_bndb = str(winja_path / dll_path.name) + ".bndb"
 # check if bndb is cache
 if Path(winja_bndb).exists():
-    dll_bv = load(winja_bndb)
+    dll_bv = load(winja_bndb, False)
     print(f"[+] Loaded bndb: {winja_bndb}")
 else:
     dll_bv = load(dll_path)
@@ -75,3 +82,7 @@ add_analysis_to_bv(bv, dll_bv, dll_path.name)
 print(f"[+] Added {dll_path.name} to bv")
 
 print("[+] Merge Finished")
+end = time.perf_counter()
+total_time = end - start
+
+print(f"[+] Completed Merge in {total_time} seconds")
